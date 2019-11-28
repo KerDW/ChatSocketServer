7ac076f4f136e72f65036a05e87265fe12405b56
Álvaro Zamora <alvarozajim@gmail.com>
2019-11-28 19:37:29 +0100

    Initial commit

---
+++ WebApplication1/Controllers/webapiControllerXD.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Web.WebSockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+public class WebSocketController : ApiController
+{
+    public HttpResponseMessage Get(string nom)
+    {
+        // La crida al websocket serà del tipus   ws://host:port/api/websocket?nom=Pere
+
+        HttpContext.Current.AcceptWebSocketRequest(new SocketHandler(nom)); return new HttpResponseMessage(HttpStatusCode.SwitchingProtocols);
+    }
+
+    private class SocketHandler : WebSocketHandler
+    {
+        private static readonly WebSocketCollection Sockets = new WebSocketCollection();
+
+        private readonly string _nom;
+
+        public SocketHandler(string nom)
+        {
+            _nom = nom;
+        }
+
+        public override void OnOpen()
+        {
+            // Quan es connecta un nou usuari: cal afegir el SocketHandler a la Collection, notificar a tothom la incorporació i donar-li la benvinguda
+            Sockets.Add(this);
+            Sockets.Broadcast(this._nom+" s'ha connectat.");
+            this.Send("Benvingut " + this._nom);
+        }
+
+        public override void OnMessage(string message)
+        {
+            // Quan un usuari envia un missatge, cal que tothom el rebi
+            Sockets.Broadcast("["+DateTime.Now.ToString("HH:mm:ss") + "] "+this._nom+": "+message);
+
+        }
+
+        public override void OnClose()
+        {
+            // Quan un usuari desconnecta, cal acomiadar-se'n, esborrar-ne el SocketHandler de la Collection i notificar a la resta que marxa
+            this.Send("Adeu " + this._nom);
+            Sockets.Remove(this);
+            Sockets.Broadcast(this._nom+" ha marxat de la conversa.");
+
+        }
+    }
+}
